49be7b6f9d5d61f50a634898f702c3a8b4bbc7e9
yash suthar <98970491+Yash636261@users.noreply.github.com>
2023-01-02 22:01:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import About from './compnents/about';
 import Navbar from './compnents/Navbar';
+// import TextForm from './compnents/textform';
 function App() {
   return (
     <>
       <Navbar title="React" />
+      <div className="container">
+        {/*<TextForm heading="enter the text here"/> */}
+        <About/>
+      </div>
+      
     </>
   );
 }
