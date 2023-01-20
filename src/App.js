cd5d68d111dd62c93e2f80f4dc63db8ca4d3f1e8
yash suthar <ysuthar@gmail.com>
2023-01-20 21:02:00 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 
 import './App.css';
+//import About from './compnents/about';
 import Navbar from './compnents/Navbar';
 import TextForm from './compnents/textform';
+import React, { useState } from 'react';
+
+
 function App() {
+  const [Mode, setMode] = useState(`dark`);
+
+  const toggleMode =()=>{
+    if(Mode==='light'){
+      setMode('dark');
+    }
+    else{
+      setMode('light');
+    }
+  }
+
   return (
     <>
-      <Navbar title="React" />
+      <Navbar title="React" mode={Mode} toggleMode={toggleMode}/>
       <div className="container">
-        <TextForm heading="enter the text here"/>
+        {<TextForm heading="enter the text here"/> }
+        {/*<About/>*/}
       </div>
+      
     </>
   );
 }
