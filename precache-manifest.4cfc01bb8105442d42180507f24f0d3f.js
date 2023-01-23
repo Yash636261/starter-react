5b543663fb33f2254cd4ad4cff33369cadb022a1
yash suthar <ysuthar@gmail.com>
2023-01-23 17:12:03 +0530

    Updates

---
+++ precache-manifest.4cfc01bb8105442d42180507f24f0d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "098d5fb3d5f1e23a62d6",
+    "url": "/starter-react/static/css/main.1ae779c4.chunk.css"
+  },
+  {
+    "revision": "098d5fb3d5f1e23a62d6",
+    "url": "/starter-react/static/js/main.098d5fb3.chunk.js"
+  },
+  {
+    "revision": "af0aac77d8aecc35095c",
+    "url": "/starter-react/static/js/1.af0aac77.chunk.js"
+  },
+  {
+    "revision": "d97e5c903f41bb420126",
+    "url": "/starter-react/static/js/2.d97e5c90.chunk.js"
+  },
+  {
+    "revision": "14edb285da93b68eb01a",
+    "url": "/starter-react/static/js/runtime~main.14edb285.js"
+  },
+  {
+    "revision": "177f8d87b89d0425d1a0fe0bfd969201",
+    "url": "/starter-react/index.html"
+  }
+];
